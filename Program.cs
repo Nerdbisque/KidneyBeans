3076472471ce3f91c3aada1a8e465f3059eeb3d2
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-10-20 13:03:30 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace cse210_ParachuteMan
 {
     class Program{
-        //Simple program to start the program by calling the StartGame method.
+        // Simple program to start the program by calling the StartGame method.
         static int Main(string[] args){
             Game game = new Game();
             game.StartGame ();
